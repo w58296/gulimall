89524d74e853dfbac2fbcf06d0f97a4f50605105
w58296 <582961331@qq.com>
2022-03-04 21:36:58 +0800

    项目初始化创建

---
+++ gulimall-user/src/test/java/com/bupt/gulimall/user/GulimallUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bupt.gulimall.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
